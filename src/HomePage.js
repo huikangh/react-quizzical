b2524d408a7034f4d1311dba08f2a60658f0edfb
Huikang Huang <huikanghuang1@gmail.com>
2022-04-27 22:37:35 -0700

    added some new files and created basic home page

---
+++ src/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function HomePage() {
+    return (
+        <div className="home--div">
+            <h1 className="home--title">Welcome to Quizzical!</h1>
+            <button className="home--button">Start</button>
+        </div>
+    )
+}
